44e3c11b5955e2b2d3c89bba4bb3dcd7749c7ce4
Filip <paliucfilip@yahoo.com>
2023-06-21 19:16:37 +0300

    Added coments to the code

--- model.py
+++ model.py
@@ -1,66 +1,62 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import os
 import torch
-import torchvision
-import torch.nn as nn # All neural network modules, nn.Linear, nn.Conv2d, BatchNorm, Loss functions
-import torchvision.datasets as datasets # Has standard datasets we can import in a nice way
-import torchvision.transforms as transforms # Transformations we can perform on our dataset
-import torch.nn.functional as F # All functions that don't have any parameters
-from torch.utils.data import DataLoader, Dataset # Gives easier dataset managment and creates mini batches
-from torchvision.datasets import ImageFolder
-import torch.optim as optim # For all Optimization algorithms, SGD, Adam, etc.
+import torchvision.transforms as transforms
 from PIL import Image
-import sys 
 from firebase import storage, database
+from torch.utils.data import DataLoader
+import os
 
-from tqdm import tqdm
-from torchvision import models
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # use gpu or cpu
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = torch.load("model.pth")
-
 model.to(device)
 
-# Check the test set
-dataset = ImageFolder("cat-and-dog/test_set/test_set/", 
-                     transform=transforms.Compose([
-                         transforms.Resize((224, 224)), 
-                         transforms.ToTensor(), 
-                         transforms.Normalize([0.5]*3, [0.5]*3)
-                     ]))
-# print(dataset)
-dataloader = DataLoader(dataset, batch_size=1, shuffle = False)
-
 def RandomImagePrediction(filepath):
+    # Deschide și convertește imaginile în format RGB
     img_array = Image.open(filepath).convert("RGB")
-    data_transforms=transforms.Compose([
+    
+    # Definirea transformărilor care vor fi aplicate asupra imaginilor
+    data_transforms = transforms.Compose([
         transforms.Resize((224, 224)), 
         transforms.ToTensor(), 
         transforms.Normalize([0.5]*3, [0.5]*3)
     ])
-    img = data_transforms(img_array).unsqueeze(dim=0) # Returns a new tensor with a dimension of size one inserted at the specified position.
+    
+    # Aplică transformările asupra imaginilor 
+    img = data_transforms(img_array).unsqueeze(dim=0)
+    
+    # Creează un loader de date pentru imaginile transformate
     load = DataLoader(img)
     
     for x in load:
-        x=x.to(device)
+        # Mută datele pe dispozitiv (GPU dacă este disponibil)
+        x = x.to(device)
+        
+        # Realizează predicții folosind modelul
         pred = model(x)
         _, preds = torch.max(pred, 1)
-        if preds[0] == 1: return (f"Dog")
-        else: return (f"Cat")
-    
+        
+        # Verifică eticheta prezisă și returnează clasa corespunzătoare
+        if preds[0] == 1:
+            return "Dog"
+        else:
+            return "Cat"
+
 while True:
     try:
         if __name__ == "__main__":
-
             image_path = 'pet_image.jpeg'
+            
+            # Descarcă imaginea din stocare
             storage.child('model/pet_image.jpeg').download(image_path)
+            
+            # Obține valoarea "compute" din baza de date care reprezintă clasificarea sau nu a imaginii animalului de companie
             compute = database.child("predictie").child("compute").get()
 
             if compute.val():
-                response = RandomImagePrediction("pet_image.jpeg") # dog image
+                # Realizează predicția imaginii
+                response = RandomImagePrediction("pet_image.jpeg") # imagine cu câine
                 print(response)
 
+                # Actualizează baza de date cu clasificarea
                 database.child("predictie").child("tip").set(response)
 
                 os.remove(image_path)
